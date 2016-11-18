7ea602286a57d3a7571c91a0538424177e574ef4
sumanla13a <lamasuman2@gmail.com>
2016-11-18 11:50:05 -0600

    project initialized

---
+++ src/main/java/com/grp11/FinalEaProjectGrp11Application.java
@@ -0,0 +1,12 @@
+package com.grp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalEaProjectGrp11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalEaProjectGrp11Application.class, args);
+	}
+}
---
+++ src/test/java/com/grp11/FinalEaProjectGrp11ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grp11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalEaProjectGrp11ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
