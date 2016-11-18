2e607d96d27dce5f427d9d69ce669323fd117892
sumanla13a <lamasuman2@gmail.com>
2016-11-18 15:08:11 -0600

    fixes pom.xml issue

---
+++ src/main/java/com/grp11/Order/OrderCtrl.java
@@ -0,0 +1,18 @@
+/*package com.grp11.Order;
+
+import java.util.Collection;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+public class OrderCtrl {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public String getAllExamples() {
+		System.out.println("here");
+		return "home2";
+	}
+}
+*/
