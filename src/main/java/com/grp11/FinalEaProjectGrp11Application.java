0b64c400843adbaa1968349652b12d28619d147b
sumanla13a <lamasuman2@gmail.com>
2016-11-18 21:07:27 -0600

    fixes view issue

--- src/main/java/com/grp11/FinalEaProjectGrp11Application.java
+++ src/main/java/com/grp11/FinalEaProjectGrp11Application.java
@@ -4,24 +4,30 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.grp11.Order.IOrder;
 import com.grp11.Order.OrderDomain;
-
+@EnableAutoConfiguration
+@Configuration
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class FinalEaProjectGrp11Application {
 	@Autowired
-	private IOrder repository;
+//	private IOrder repository;
 	public static void main(String[] args) {
 		
 		SpringApplication.run(FinalEaProjectGrp11Application.class, args);
 	}
-	@PostConstruct
+	//	TODO: create the first admin from here check if already exists first
+	/*@PostConstruct
 	public void init() {
 		OrderDomain o = new OrderDomain();
 		o.setPrice(1);
 		o.setQuantity(2);
 		repository.save(o);
-	}
+	}*/
 }
