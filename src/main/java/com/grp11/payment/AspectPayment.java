999d1a928ed2971a34da3ef4ffdd45a33053c8be
sumanla13a <lamasuman2@gmail.com>
2016-11-22 18:05:31 -0600

    aop email sending

--- src/main/java/com/grp11/payment/AspectPayment.java
+++ src/main/java/com/grp11/payment/AspectPayment.java
@@ -4,8 +4,11 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //import org.springframework.web.client.RestTemplate;
+
+import com.grp11.util.IEmail;
 
 //import com.grp11.util.Email;
 //import com.grp11.util.ResponseObj;
@@ -15,10 +18,12 @@ import org.springframework.stereotype.Component;
 public class AspectPayment {
 //	private static final String ServiceURL = "http://localhost:3001/card_validation/isvalid?cardNumber=";
 //	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	private IEmail e;
+
 	@After("execution(* com.grp11.payment.PaymentCtrl.*Payment(..)) and args(Payment, ..)")
 	public void sendEmail(JoinPoint jp, PaymentDomain Payment) throws Throwable {
-		/*Email e = new Email();
-		e.send("lamasuman00114@gmail.com");*/
+		e.send("lamasuman00114@gmail.com");
 		System.out.println("Let us imagine we are sending email here");
 	}
 	@After("execution(* com.grp11.payment.PaymentCtrl.payNow(..)) and args(UserId, ..)")
